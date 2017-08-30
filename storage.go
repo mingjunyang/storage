ba096495319aecf498bdaf0275f23958914cbe42
Xiaoming <xiaoming.yang@mbp.local>
2017-08-30 15:06:19 +0800

    remove remote storage

--- storage.go
+++ storage.go
@@ -62,19 +62,6 @@ type FS interface {
 	Delete(ctx context.Context, path string) error
 }
 
-// FSFromURL takes a file system path and returns a FSWalker
-// corresponding to a supported storage system (CloudStorage,
-// S3, or Local if no platform-specific prefix is used).
-func FSFromURL(path string) FS {
-	if strings.HasPrefix(path, "gs://") {
-		return &CloudStorage{Bucket: strings.TrimPrefix(path, "gs://")}
-	}
-	if strings.HasPrefix(path, "s3://") {
-		return &S3{Bucket: strings.TrimPrefix(path, "s3://")}
-	}
-	return Local(path)
-}
-
 // Prefix creates a FS which wraps fs and prefixes all paths with prefix.
 func Prefix(fs FS, prefix string) FS {
 	return pfx{
